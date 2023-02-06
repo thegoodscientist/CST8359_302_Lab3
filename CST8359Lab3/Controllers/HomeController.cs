10dae38e838301db6aaf6ac5bf4015e9d9f41910
thegoodscientist <salimianhanieh@gmail.com>
2023-02-06 12:26:02 -0500

    Completed up until Step 5

---
+++ CST8359Lab3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CST8359Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            // you will complete this
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            // you will complete this
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
